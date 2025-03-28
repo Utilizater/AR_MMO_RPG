e33cc187833a13dd3e3eb4ecce8d9fd3205caf36
Danilggw <ggwtechsf@gmail.com>
2025-03-27 20:41:37 -0400

    add ai

---
+++ app.config.js
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+
+export default {
+  name: 'AR Fantasy Game',
+  slug: 'ar-fantasy-game',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#FFFFFF',
+    },
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
+  extra: {
+    openAiApiKey: process.env.OPEN_AI_API_KEY,
+  },
+};
